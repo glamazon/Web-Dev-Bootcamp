ec105de0e9d04ad481aac0a9635716a31713b7b2
Peggy Sturman <glamazon5@gmail.com>
2016-12-15 23:03:36 -0800

    Refactored code and created functions

--- ToDoList/list.js
+++ ToDoList/list.js
@@ -5,14 +5,38 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
 	//handle input
 	if(input === "list"){
-		console.log(todos);
-	} else if(input === "new") {
-		//ask for new todo
-		var newTodo = prompt("Enter new todo")
-		//add to tods array
-		todos.push(newTodo);
-}
+		listTodos();
+	} 	else if(input === "new") {
+		addTodo();
+	}	else if(input === "delete"){
+		deleteTodo();
+	}
 	//ask again for new input
 	input = prompt("What would you like to do?");
 }
 console.log("OKAY, YOU QUIT THE APP");
+
+function listTodos(){
+	console.log("**********")
+	todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+		console.log("**********")
+}
+
+function addTodo(){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo")
+	//add to tods array
+	todos.push(newTodo);
+	console.log("Added Todo")
+}
+
+function deleteTodo(){
+	//ask for index of todo to be deleted
+	var index = prompt ("Enter index of todo to delete");
+	//delete that todo
+	//splice()
+	todos.splice(index,1);
+	console.log("Deleted Todo");
+}
