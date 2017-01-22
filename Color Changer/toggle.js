31dc10c0bd0740c5f42e08a151b3d82993c25cc2
Peggy Sturman <glamazon5@gmail.com>
2017-01-21 23:03:17 -0800

    Toggle background color with each button click
    Toggle background color with each button click.

---
+++ Color Changer/toggle.js
@@ -0,0 +1,8 @@
+var button = document.querySelector ("button");
+var isPurple = false;
+
+button.addEventListener("click", function(){
+	if(isPurple) {
+		document.body.classList.toggle("purple");
+		
+	});
